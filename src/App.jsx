ca5d9d6a1b985d86ad0dfb15d44cea18f0720e92
ildafadli16 <ildafadli16@mail.ugm.ac.id>
2022-10-31 13:49:00 +0700

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import { Button, Card, Navbar } from "./components";
-import { BathulMasail, Login, NewPost, NewQuestion, Notification, Profile, Splash } from "./pages";
+import {
+  BathulMasail,
+  Login,
+  NewPost,
+  NewQuestion,
+  Notification,
+  Profile,
+  Splash,
+} from "./pages";
 
 const App = () => {
   return (
     <>
-      <Splash />
-      <Login />
+      <Navbar />
     </>
   );
 };
