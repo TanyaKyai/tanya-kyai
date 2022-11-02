841fd00d2b9913cb641a535a6b12906e63a17055
Damar Buana Murti <damarbm30@gmail.com>
2022-11-02 09:34:21 +0700

    modify rendered component on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
     <>
       <GoogleOAuthProvider clientId="262788619795-odstb3g9l2l5i265rkrisqf2m6kd4dl3.apps.googleusercontent.com">
         <Login />
-        <Home />
+        {/* <Home /> */}
       </GoogleOAuthProvider>
     </>
   );
