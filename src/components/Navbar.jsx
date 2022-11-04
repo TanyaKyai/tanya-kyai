4f211cde89dacbef124bf4c873cedc23ae6902c6
ildafadli16 <ildafadli16@mail.ugm.ac.id>
2022-11-04 13:41:51 +0700

    interactive color of active navbar link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 import { book, create, home, notification, profile } from "../assets";
 import { userRole } from "../services/auth";
@@ -59,6 +59,9 @@ const adminNav = [
   },
 ];
 
+const activeLink = " text-primary ";
+const normalLink = "";
+
 const Navbar = () => {
   return (
     <>
@@ -66,20 +69,32 @@ const Navbar = () => {
         <div className="fixed bottom-0 flex w-full  justify-evenly rounded-t-3xl bg-white drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)] sm:w-[476px]">
           {userRole() === "admin"
             ? adminNav.map((item, index) => (
-                <Link to={item.link} key={index}>
+                <NavLink
+                  to={item.link}
+                  key={index}
+                  className={({ isActive }) =>
+                    isActive ? activeLink : normalLink
+                  }
+                >
                   <button className="flex flex-col items-center justify-center pt-2">
                     <img className="h-[25px] w-[25px]" src={item.img} />
                     <p className="text-[12px]">{item.name}</p>
                   </button>
-                </Link>
+                </NavLink>
               ))
             : userNav.map((item, index) => (
-                <Link to={item.link} key={index}>
+                <NavLink
+                  to={item.link}
+                  key={index}
+                  className={({ isActive }) =>
+                    isActive ? activeLink : normalLink
+                  }
+                >
                   <button className="flex flex-col items-center justify-center pt-2">
                     <img className="h-[25px] w-[25px]" src={item.img} />
                     <p className="text-[12px]">{item.name}</p>
                   </button>
-                </Link>
+                </NavLink>
               ))}
         </div>
       </nav>
