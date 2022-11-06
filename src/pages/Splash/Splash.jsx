5c8a185398cc25d1fd3f5508910314a3b80a7312
Damar Buana Murti <damarbm30@gmail.com>
2022-11-06 17:04:37 +0700

    update splash screen logo

--- src/pages/Splash/Splash.jsx
+++ src/pages/Splash/Splash.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { logoWhite, bookWhite, tripleDiamonds } from "../../assets";
+import { logoWhite, tripleDiamonds, fontLogoWhite } from "../../assets";
 
 const Splash = () => {
   return (
@@ -8,20 +8,19 @@ const Splash = () => {
         Lewati
       </Link>
       <div className="absolute top-36 flex flex-col items-center justify-center ">
-        <img src={bookWhite} alt="logo" className="my-4" />
         <img src={logoWhite} alt="logo" className="w-[100px]" />
-        <h2 className="font-milliard text-2xl  text-white">Tanya Kyai</h2>
+        <img src={fontLogoWhite} alt="logo" className="my-4" />
       </div>
       <div className="absolute bottom-24 flex flex-col items-center justify-center gap-y-3">
         <img src={tripleDiamonds} alt="kutip" className="w-[15px]" />
-        <p className=" flex w-[80%] max-w-xl text-center font-inter text-[9px] text-white">
+        <p className="flex px-8 text-center font-inter text-[11px] text-white md:w-3/4 md:text-sm lg:w-1/2">
           Aplikasi (Al-Bahtsu) merupakan aplikasi di bawah naungan Lembaga Bahtsul Masail (LBM) PWNU DIY sebagai respon
           atas perkembangan laju era digital yang semakin cepat dan berkembang. Tujuan dari aplikasi ini adalah
           membangun komunikasi yang intens dan berkelanjutan atas permasalahan keagamaan, muamalat, dan keluarga yang
           terjadi di lingkup masyarakat secara luas dan nahdiyyin pada khususnya
         </p>
       </div>
-      <footer className="absolute bottom-4 font-inter text-xs text-white">
+      <footer className="absolute bottom-4 font-inter text-xs text-white md:text-sm">
         Powered by <span className="font-bold">PWNU DIY</span>
       </footer>
     </section>
