4075b93a381015e1d409c710e2c45abec1919d6e
Damar Buana Murti <damarbm30@gmail.com>
2022-11-07 11:44:16 +0700

    create coming soon text

--- src/pages/BahtsulMasail/BahtsulMasail.jsx
+++ src/pages/BahtsulMasail/BahtsulMasail.jsx
@@ -1,5 +1,9 @@
 const BahtsulMasail = () => {
-  return <div>BahtsulMasail</div>;
+  return (
+    <section className="flex h-screen w-full items-center justify-center">
+      <h1 className="font-roboto text-5xl font-bold uppercase text-primary">Coming Soon</h1>
+    </section>
+  );
 };
 
 export default BahtsulMasail;
