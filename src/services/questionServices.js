fc7203bba80aac0a1ed0ba5199f36d41ae424de8
Damar Buana Murti <damarbm30@gmail.com>
2022-11-15 19:29:04 +0700

    add datetime formatting

--- src/services/questionServices.js
+++ src/services/questionServices.js
@@ -1,8 +1,11 @@
+import { format } from "date-fns";
+
 import api from "./baseUrl";
 
 export const createQuestion = async (data) => {
   try {
-    const newQuestion = data;
+    const newDate = format(new Date(), "dd MMM yyyy p");
+    const newQuestion = { ...data, newDate };
     const response = await api.post("/questions", newQuestion);
   } catch (error) {
     console.log(error);
