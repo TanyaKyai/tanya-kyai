f71f7b24c19a968de7eb34aee680c90f1d1012d8
Damar Buana Murti <damarbm30@gmail.com>
2022-11-15 19:07:17 +0700

    change back button to inline-block

--- src/pages/PostDetail/PostDetail.jsx
+++ src/pages/PostDetail/PostDetail.jsx
@@ -10,7 +10,7 @@ const PostDetail = ({ posts }) => {
 
   return (
     <section className="mx-auto px-8 pt-12 md:w-3/4 lg:w-1/2">
-      <Link to="/home">
+      <Link to="/home" className="inline-block">
         <img src={leftArrow} alt="left-arrow" />
       </Link>
       <div className="mt-12 rounded-2xl bg-gray px-5 py-4">
@@ -47,7 +47,14 @@ const PostDetail = ({ posts }) => {
           </div>
           <div className="flex items-center">
             <button>
-              <img src={share} alt="share" className="h-[16px] w-[16px]" />
+              <img
+                src={share}
+                alt="share"
+                className="h-[16px] w-[16px]"
+                onClick={() => {
+                  navigator.clipboard.writeText(window.location.href);
+                }}
+              />
             </button>
           </div>
         </div>
