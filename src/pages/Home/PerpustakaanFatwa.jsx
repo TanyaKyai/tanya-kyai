dda0c7eb18a8514a3343cc78b4f964fdb53ffe6b
Damar Buana Murti <damarbm30@gmail.com>
2022-11-19 17:35:06 +0700

    add styling

--- src/pages/Home/PerpustakaanFatwa.jsx
+++ src/pages/Home/PerpustakaanFatwa.jsx
@@ -2,10 +2,10 @@ import Fatwa from "./Fatwa";
 
 const PerpustakaanFatwa = ({ fatwas }) => {
   return (
-    <div>
+    <div className="mt-4 flex max-h-[75vh] flex-col gap-4 overflow-y-auto md:px-5">
       {fatwas.length
         ? fatwas.map((fatwa) => <Fatwa key={fatwa.id} fatwa={fatwa} />)
-        : "Belum ada fatwa untuk ditampilkan"}
+        : "Tidak ada fatwa untuk ditampilkan"}
     </div>
   );
 };
