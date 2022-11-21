d516cb3d20fa50b59669bfeb9f47461a9563753a
Damar Buana Murti <damarbm30@gmail.com>
2022-11-21 10:12:28 +0700

    remove console.log

--- src/pages/QuestionList/Question.jsx
+++ src/pages/QuestionList/Question.jsx
@@ -10,8 +10,6 @@ const Question = ({ setActiveQuestion, questionItem }) => {
     navigate("/new-post");
   };
 
-  console.log(questionItem);
-
   return (
     <div className="mb-4 flex max-h-[75vh] max-w-2xl flex-col rounded-2xl bg-gray px-5 pt-4 pb-2 last:mb-0">
       <p>{question}</p>
