dc1b973937aaa303ead5a14d7d91ac6d21a35fa3
ildafadli16 <ildafadli16@mail.ugm.ac.id>
2022-12-01 10:44:25 +0700

    update size logo

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
   return (
     <section className="h-screen w-full">
       <div className="absolute top-1/2 left-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-4">
-        <img src={logoHijau} alt="book-logo" className=" mb-12 w-[150px]" />
+        <img src={logoHijau} alt="book-logo" className=" mb-12 w-[70px]" />
 
         <button
           className="rounded-md bg-primary px-8 py-1 font-roboto text-[13px] font-bold text-white"
