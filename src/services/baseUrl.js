e5787d629dcdd01493b077bef3fe67cfd2fed89a
Damar Buana Murti <damarbm30@gmail.com>
2022-12-01 12:36:56 +0700

    change URL

--- src/services/baseUrl.js
+++ src/services/baseUrl.js
@@ -1,3 +1,3 @@
 import axios from "axios";
 
-export default axios.create({ baseURL: "https://c05e-103-162-62-46.ap.ngrok.io" });
+export default axios.create({ baseURL: "https://95fb-182-2-68-213.ap.ngrok.io" });
