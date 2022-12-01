b155099080c63babd99df0b6b94c3cbc6f49edbc
ildafadli16 <ildafadli16@mail.ugm.ac.id>
2022-12-01 11:06:15 +0700

    update ui logo splash login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -16,9 +16,14 @@ const Login = () => {
 
   return (
     <section className="h-screen w-full">
-      <div className="absolute top-1/2 left-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-4">
-        <img src={logoHijau} alt="book-logo" className=" mb-12 w-[70px]" />
-
+      <div className="absolute top-1/2 left-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center">
+        <img src={logoHijau} alt="book-logo" className="  w-[150px]" />
+        <p className="  font-roboto text-[23px] font-bold text-primary">
+          Tanya Kyai
+        </p>
+        <p className="mb-5 font-roboto text-[9px] text-primary">
+          Powered <span className="font-bold">By LBM PWNU DIY</span>
+        </p>
         <button
           className="rounded-md bg-primary px-8 py-1 font-roboto text-[13px] font-bold text-white"
           onClick={() => handleLogin()}
--- src/pages/Splash/Splash.jsx
+++ src/pages/Splash/Splash.jsx
@@ -11,17 +11,18 @@ const Splash = () => {
         Lewati
       </Link>
       <div className="absolute top-36 flex flex-col items-center justify-center ">
-        <img src={logoPutih} alt="logo" className="w-[70px]" />
+        <img src={logoPutih} alt="logo" className="w-[180px]" />
+        <p className="pt-2 font-roboto text-[23px] font-bold text-white">
+          Tanya Kyai
+        </p>
       </div>
       <div className="absolute bottom-24 flex flex-col items-center justify-center gap-y-3">
         <img src={tripleDiamonds} alt="kutip" className="w-[15px]" />
         <p className="flex px-8 text-center font-inter text-[11px] text-white md:w-3/4 md:text-sm lg:w-1/2">
-          Aplikasi (Al-Bahtsu) merupakan aplikasi di bawah naungan Lembaga
-          Bahtsul Masail (LBM) PWNU DIY sebagai respon atas perkembangan laju
-          era digital yang semakin cepat dan berkembang. Tujuan dari aplikasi
-          ini adalah membangun komunikasi yang intens dan berkelanjutan atas
-          permasalahan keagamaan, muamalat, dan keluarga yang terjadi di lingkup
-          masyarakat secara luas dan nahdiyyin pada khususnya
+          Aplikasi (Al-Bahtsu) merupakan aplikasi yang bertujuan untuk membangun
+          komunikasi yang intens dan berkelanjutan atas permasalahan keagamaan,
+          muamalat, dan keluarga yang terjadi di lingkup masyarakat secara luas
+          dan nahdiyyin pada khususnya.
         </p>
       </div>
       <footer className="absolute bottom-4 font-inter text-xs text-white md:text-sm">
