e7195ff3e95652ca03411d6e12729b4de5e0119b
gaungalif <gaungalif@mail.ugm.ac.id>
2022-12-09 10:46:26 +0700

    gaung

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -9,8 +9,6 @@ const Profile = () => {
 
   const navigate = useNavigate();
 
-  console.log("Picture: ", picture);
-
   return (
     <section className="mx-auto w-full px-4 md:w-1/2">
       <h1 className="pt-6 text-center font-roboto text-xl font-bold ">Profile</h1>
--- src/services/baseUrl.js
+++ src/services/baseUrl.js
@@ -1,3 +1,3 @@
 import axios from "axios";
 
-export default axios.create({ baseURL: "http://127.0.0.1:5000" });
+export default axios.create({ baseURL: "http://10.113.64.217:5000" });
