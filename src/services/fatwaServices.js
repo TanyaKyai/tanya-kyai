24ebf594ac79025df14e68bffa01fabdeb77fb60
Damar Buana Murti <damarbm30@gmail.com>
2022-12-11 21:03:12 +0700

    add getFatwas and createFatwa function

--- src/services/fatwaServices.js
+++ src/services/fatwaServices.js
@@ -2,8 +2,29 @@ import api from "./baseUrl";
 
 export const getFatwas = async (setFatwas) => {
   try {
-    // const response = await api.get("/fatwas");
-    // setFatwas(response.data);
+    const response = await api.get("/fatwas");
+    setFatwas(response.data.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const createFatwa = async (setFatwas) => {
+  try {
+    const fd = new FormData();
+
+    for (const [key, value] of Object.entries(data)) {
+      if (typeof value === "object") {
+        fd.append(key, value[0]);
+      } else {
+        fd.append(key, value);
+        console.log(value);
+      }
+    }
+
+    const response = await api.post("/file-upload", fd);
+    console.log(response);
+    getFatwas(setFatwas);
   } catch (error) {
     console.log(error);
   }
