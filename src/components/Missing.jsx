6c1114b694f6e9f97df05990638ddb6bfd01e52f
Damar Buana Murti <damarbm30@gmail.com>
2022-12-12 05:28:21 +0700

    update Missing component

--- src/components/Missing.jsx
+++ src/components/Missing.jsx
@@ -8,6 +8,9 @@ const Missing = () => {
       <h1 className="text-center font-roboto text-5xl font-bold uppercase text-primary">
         Error 404 <br /> Page Not Found
       </h1>
+      <h1 className="mt-4 text-center font-roboto text-xl font-bold uppercase text-primary">
+        Mohon login apabila Anda belum login
+      </h1>
       <button
         className="mx-auto mt-6 rounded-md bg-primary px-8 py-1 font-roboto text-base font-bold text-white"
         onClick={() => navigate("/home")}
