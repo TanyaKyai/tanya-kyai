4bcf8b1855c30bc23d019286ef59a87d088b36a3
Damar Buana Murti <damarbm30@gmail.com>
2022-12-12 04:53:35 +0700

    pass setFatwas props

--- src/pages/Home/PerpustakaanFatwa.jsx
+++ src/pages/Home/PerpustakaanFatwa.jsx
@@ -1,10 +1,12 @@
 import Fatwa from "./Fatwa";
 
-const PerpustakaanFatwa = ({ fatwas }) => {
+const PerpustakaanFatwa = ({ fatwas, setFatwas }) => {
+  console.log("Perpus: ", fatwas);
+
   return (
     <div className="mt-4 flex max-h-[75vh] flex-col gap-4 overflow-y-auto md:px-5">
       {fatwas.length
-        ? fatwas.map((fatwa) => <Fatwa key={fatwa.id} fatwa={fatwa} />)
+        ? fatwas.map((fatwa) => <Fatwa key={fatwa.id} fatwa={fatwa} setFatwas={setFatwas} />)
         : "Tidak ada fatwa untuk ditampilkan"}
     </div>
   );
