67368cd37b058b42c019d3426cf30e7f94ce304b
ildafadli16 <ildafadli16@mail.ugm.ac.id>
2022-12-13 16:31:14 +0700

    add ketentuan pertanyaan

--- src/pages/NewQuestion/NewQuestion.jsx
+++ src/pages/NewQuestion/NewQuestion.jsx
@@ -19,8 +19,13 @@ const NewQuestion = () => {
     <section>
       {!isToggled ? (
         <div className="mx-auto px-4 md:w-3/4 lg:w-1/2">
-          <div className="pt-6 text-center font-roboto text-xl font-bold">Tanya Kyai</div>
-          <form onSubmit={handleSubmit(onSubmit)} className="mt-12 flex flex-col gap-4">
+          <div className="pt-6 text-center font-roboto text-xl font-bold">
+            Tanya Kyai
+          </div>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="mt-12 flex flex-col gap-4"
+          >
             <div className="flex flex-col">
               <textarea
                 className="h-[200px] rounded-2xl bg-gray py-2 px-4 outline-none"
@@ -28,7 +33,35 @@ const NewQuestion = () => {
                 placeholder="Isi pertanyaan"
                 {...register("isi", { required: true })}
               />
-              {errors.isi && <span className="mt-2 font-roboto text-red-500">Pertanyaan tidak bisa kosong</span>}
+              {errors.isi && (
+                <span className="mt-2 font-roboto text-red-500">
+                  Pertanyaan tidak bisa kosong
+                </span>
+              )}
+
+              <span className="mt-4 mb-3 text-[10px]">
+                Ketentuan pengiriman pertanyaan
+              </span>
+
+              <ol className=" list-decimal pl-4 text-justify text-[10px]">
+                <li>
+                  Nama penanya akan tampil secara anonim, pihak LBM PWNU DIY
+                  selaku penjawab pertanyaan tidak akan mengetahui identitas
+                  penanya.
+                </li>
+                <li>
+                  Setiap akun tanyakyai.id dapat memberikan tanggapan pada kolom
+                  komentar setiap jawaban dan postingan dari Admin tanyakyai.id.
+                  Nama pemberi tanggapan (komentar) akan muncul sesuai dengan
+                  akun google yang digunakan.
+                </li>
+                <li>
+                  Setiap pertanyaan akan dijawab dalam kurun waktu 3 - 7 hari
+                  setelah pertanyaan diajukan melalui fitur pertanyaan ini.
+                  Mohon rutin membuka aplikasi untuk memantau jawaban atas
+                  pertanyaan yang telah diajukan.
+                </li>
+              </ol>
             </div>
             <button
               className="mx-auto mt-2 rounded-md bg-primary px-8 py-1 font-roboto text-base font-bold text-white"
